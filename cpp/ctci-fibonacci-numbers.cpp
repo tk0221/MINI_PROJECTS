d4c434d5039726422dcc4eb15482ebb3834bc9a3
tk0221 <taehun.kim@sportngin.com>
2016-12-30 17:15:37 -0600

    space

--- cpp/ctci-fibonacci-numbers.cpp
+++ cpp/ctci-fibonacci-numbers.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int fibonacci(int n) {
   if (n == 0) return 0;
   if (n == 1) return 1;
-  return fibonacci(n-1)+ fibonacci(n-2);
+  return fibonacci(n-1) + fibonacci(n-2);
 }
 
 int main() {
