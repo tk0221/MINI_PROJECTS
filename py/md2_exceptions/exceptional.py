1cc93dc97946141bf6432d65b44ea73dca348b8e
Taehun Kim <kimx2815@umn.edu>
2017-01-15 20:53:49 -0600

    python exceptions

---
+++ py/md2_exceptions/exceptional.py
@@ -0,0 +1,11 @@
+import sys
+
+def convert(s):
+    '''Convert to an integer'''
+    try:
+        return int(s)
+    except (ValueError, TypeError) as e:
+        print("Conversion error {}"\
+            .format(str(e)), 
+            file=sys.stderr)
+        raise
