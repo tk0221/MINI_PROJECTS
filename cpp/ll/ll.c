33ff6726cb94dab4b682f68f7bc165064102fbf6
tk0221 <kimx2815@umn.edu>
2017-10-24 20:52:40 -0500

    test

---
+++ cpp/ll/ll.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _NODE {
+  char Data;
+  struct _NODE *Next;
+} NODE;
+
+NODE *head, *end, *temp;
+NODE *temp1, *temp2, *temp3, *temp4;
+
+void Initialize(void);
+void InsertNode(NODE *);
+
+
+int main(){
+  return 0;
+}
+
+
