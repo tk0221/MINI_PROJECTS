893e736a740cf475b5752cace8c015fb7e9c6df8
Taehun Kim <kimx2815@umn.edu>
2018-02-27 15:24:06 -0800

    fix erros

--- coding_prac/algo/l04.rb
+++ coding_prac/algo/l04.rb
@@ -1,44 +1,46 @@
-
-
-
-arr = [16,14,10,8,7,9,3,2,4,1]
+before = [4,1,3,2,16,9,10,14,8,7]
+after = [16,14,10,8,7,9,3,2,4,1]
 
 def root(arr)
-    return arr[0]
+    return arr.first
 end
 
 def parent(arr, i)
     return arr[i/2]
 end
 
-def left(arr, i)
-    return arr[(2*i)-1]
+def left(i)
+    return 2*i
 end
 
-def right(arr, i)
-    return arr[(2*i+1)-1]
+def right(i)
+    return 2*i+1
 end
 
 def max_heapify(arr, i)
-    l = 2*i-1
-    r = 2*i
-    large = arr[i]
-    if l < arr.size && arr[l] > arr[i]
-        large = arr[l]
+    l = left(i)
+    r = right(i)
+    largest = i
+    
+    if l <= arr.size && arr[l-1] > arr[i-1]
+        largest = l
     end
-    if r < arr.size && arr[r] > large
-        large = arr[r]
+
+    if r && r <= arr.size && arr[r-1] > arr[largest-1]
+        largest = r
     end
-    if large != arr[i]
-        tmp = arr[i]
-        arr[i] = large
-        large == arr[l] ? arr[l] = tmp : arr[r] = tmp
+
+    if largest != i
+        arr[i-1], arr[l-1] = arr[l-1], arr[i-1] if largest == l
+        arr[i-1], arr[r-1] = arr[r-1], arr[i-1] if largest == r
+        arr = max_heapify(arr, largest)
     end
     return arr
+
 end
 
 def build_max_heap(arr)
-    (arr.size/2).downto(0) do |i|
+    ((arr.size)/2).downto(1) do |i|
         arr = max_heapify(arr, i)
     end
     return arr
