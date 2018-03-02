118dab03f98e2c604c1b5408247c26fc9196deb5
Taehun Kim <kimx2815@umn.edu>
2018-03-01 20:32:53 -0800

    update and add comments

--- coding_prac/ruby/subsets.rb
+++ coding_prac/ruby/subsets.rb
@@ -1,44 +1,18 @@
-
-
-
-
+# TLE 
 def subsets(nums, ans = [])
     ans << nums unless ans.include?(nums)
     (0...nums.size).each do |i|
        subsets(nums-[nums[i]], ans)
     end
     return ans
-    
-
-
-
-end
-
-
-def subsets2(arr)
-
-  return [[]] if arr.empty?
-
-  old_subsets = subsets(arr.drop(1))
-
-  new_subsets = []
-
-  old_subsets.each do |subset|
-
-    new_subsets << subset + [arr.first]
-
-  end
-
-  old_subsets + new_subsets
-
 end
 
 #iter
-def subsets3(arr)
+def subsets2(arr)
     return arr if arr.nil?
     ans = []
     (0...1<<arr.size).each do |i|
-        b = ("%b" % i).rjust(arr.size, '0')
+        b = ("%b" % i).rjust(arr.size, '0') #rjust "0" -> "0000"
         tmp = []
         (0...b.size).each do |j|
             tmp << arr[j] if b[j] == "1"
