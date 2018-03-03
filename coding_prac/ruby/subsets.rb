085d3512cf5e2acc904f99bdf36e11a9713713ec
Taehun Kim <kimx2815@umn.edu>
2018-03-02 17:28:05 -0800

    little faster than subests2

--- coding_prac/ruby/subsets.rb
+++ coding_prac/ruby/subsets.rb
@@ -23,6 +23,24 @@ def subsets2(arr)
     return ans
 end
 
+def subsets2_1(arr)
+#separate cleating b string and access it
+    return arr if arr.nil?
+    str_arr = []
+    result = []
+    (0...1<<arr.size).each do |i|
+        str_arr << ("%b" % i).rjust((arr.size), '0')
+    end
+    str_arr.each do |b|
+        tmp = []
+        (0...b.size).each do |j|
+            tmp << arr[j] if b[j] == "1"
+        end
+        result << tmp
+    end
+    return result
+end
+
 
 def subsets3(nums)
     return [[]] if nums.empty?
@@ -40,9 +58,17 @@ arr = [1,3,5,7,9,2,4,6,8,10,11,12,13,14,15,16,17]
 start = Time.now
 #subsets(arr) #TLE
 p (Time.now - start)*1000
+
+
 start = Time.now
 subsets2(arr)
 p (Time.now - start)*1000
+
+start = Time.now
+subsets2_1(arr)
+p (Time.now - start)*1000
+
+
 start = Time.now
 subsets3(arr)
 p (Time.now - start)*1000
